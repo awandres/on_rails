171b246f6aec8b0bdafae3e04b31fbb9cd396c82
Alex Wandres <alex.r.wandres@gmail.com>
2017-07-26 21:48:58 -0400

    CR--

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,32 @@
 class ArticlesController < ApplicationController
 
-  def new
+  def index
+    @articles = Article.all
   end
 
+  def new
+    @article = Article.new
+  end
+  
+  def edit
+  @article = Article.find(params[:id])
+  end
   def create
     @article = Article.new(article_params)
 
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
-  def private
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  private
+  def article_params
     params.require(:article).permit(:text, :title)
   end
 end
